cee5692b0c8d3ec8cddf7e44cb042eab3b4b4828
Jo Lares <jo_lares@ultimatesoftware.com>
2019-07-03 13:09:55 -0400

    backend-api (feat): Add Widgets controller

--- backend-api/src/app.module.ts
+++ backend-api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WidgetsController } from './widgets/widgets.controller';
+import { WidgetsService } from './widgets/widgets.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, WidgetsController],
+  providers: [AppService, WidgetsService],
 })
 export class AppModule {}
--- backend-api/src/main.ts
+++ backend-api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
+
+  // Cors config.
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
   await app.listen(3000);
 }
 bootstrap();
