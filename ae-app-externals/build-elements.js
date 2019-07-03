da7898dff9df7ddad35a89c4a406bae7babf129c
Alicia Rodriguez Taboada <alicia_rodriguez@ultimatesoftware.com>
2019-07-03 13:09:28 -0400

    hook up angular element to angular app

--- ae-app-externals/build-elements.js
+++ ae-app-externals/build-elements.js
@@ -3,26 +3,15 @@ const concat = require('concat');
 (async function build() {
   // TODO: We need to bundle all the JS in a single js chunk....
   const files = [
-    './dist/ips-window/2.js',
-    './dist/ips-window/7.js',
-    './dist/ips-window/8.js',
-    './dist/ips-window/9.js',
-    './dist/ips-window/10.js',
-    './dist/ips-window/11.js',
-    './dist/ips-window/12.js',
-    './dist/ips-window/21.js',
-    './dist/ips-window/25.js',
-    './dist/ips-window/31.js',
-    './dist/ips-window/43.js',
-    './dist/ips-window/45.js',
-    './dist/ips-window/76.js',
-    './dist/ips-window/common.js',
-    './dist/ips-window/runtime.js',
-    './dist/ips-window/polyfills.js',
-    './dist/ips-window/scripts.js',
-    './dist/ips-window/main.js',
+    // './dist/ae-app/common.js',
+    './dist/ae-app/runtime-es5.js',
+    './dist/ae-app/runtime-es2015.js',
+    './dist/ae-app/polyfills-es5.js',
+    './dist/ae-app/polyfills-es2015.js',
+    './dist/ae-app/scripts.js',
+    './dist/ae-app/main-es5.js',
+    './dist/ae-app/main-es2015.js'
   ]
   await fs.ensureDir('elements')
-  await concat(files, 'elements/ips-window.js');
-  await fs.copyFile('./dist/ips-window/styles.css', 'elements/styles.css')
+  await concat(files, 'elements/ae-widget1.js');
 })()
